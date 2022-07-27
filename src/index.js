f5e234cb95b3f01929361076fc3fba5558fa0b81
AnnaFeona <glavatskikh.anna@gmail.com>
2022-07-27 18:53:02 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    let tmp = '' + Math.abs(n);
+    for (let i= tmp.length - 1; i >= 0; i--) {
+        result += tmp[i];
+    }
+    return +result;
 }
